7de72b2b2d056e00b243ee595b09c1eb0b225506
Andrés Clúa <andresclua@gmail.com>
2022-12-13 12:04:25 +0100

    starter version

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const breakpoints = [
+    {            
+        'mobile': 580,
+    },
+    {            
+        'tablets': 810,
+    },
+    {            
+        'tabletm': 1024,
+    },
+    {            
+        'tabletl': 1300,
+    },
+    {            
+        'laptop': 1570
+    },
+    {            
+        'desktop': 1700
+    }
+]
+
+
+export {breakpoints};
