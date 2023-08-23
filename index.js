9d6cbc29834dfb54bfa1286cab49097d32e7c7c3
Andrés Clúa <andresclua@gmail.com>
2023-08-23 15:21:15 +0200

    update breakpoints

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-
 const breakpoints = [
-    {            
-        'mobile': 580,
-    },
-    {            
-        'tablets': 810,
-    },
-    {            
-        'tabletm': 1024,
-    },
-    {            
-        'tabletl': 1300,
-    },
-    {            
-        'laptop': 1570
-    },
-    {            
-        'desktop': 1700
+    { 'mobile': 580 },
+    { 'tablets': 810 },
+    { 'tabletm': 1024 },
+    { 'tabletl': 1300 },
+    { 'laptop': 1570 },
+    { 'desktop': 1700 }
+];
+
+function getCurrentBreakpoint(screenWidth) {
+    for (let i = breakpoints.length - 1; i >= 0; i--) {
+        const breakpoint = breakpoints[i];
+        const breakpointName = Object.keys(breakpoint)[0];
+        const breakpointValue = breakpoint[breakpointName];
+
+        if (screenWidth >= breakpointValue) {
+            return breakpointName;
+        }
     }
-]
 
+    // If the screen width is smaller than all breakpoints, return a default value
+    return 'mobile';
+}
 
-export {breakpoints};
+function updateBreakpointOnResize() {
+    const screenWidth = window.innerWidth;
+    return getCurrentBreakpoint(screenWidth);
+}
+
+// Add event listener for screen resize
+window.addEventListener('resize', updateBreakpointOnResize);
+
+// Initial check for breakpoint when the page loads
+updateBreakpointOnResize();
+
+export { breakpoints, getCurrentBreakpoint, updateBreakpointOnResize };
